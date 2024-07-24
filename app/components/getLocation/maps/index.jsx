7924f035180f5faffa4dd668615a2c163323d799
Mohammed Ebesh <ebeshaaaa@gmail.com>
2024-07-24 18:33:13 +0300

    m

--- app/components/getLocation/maps/index.jsx
+++ app/components/getLocation/maps/index.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
